a2935709157feceb36d68a0deec3c2356af51837
User021230 <mgundo48@gmail.com>
2023-07-25 23:02:24 +0200

    Create function getComputerChoice in Script file

---
+++ script.js
@@ -0,0 +1,17 @@
+function getcomputerChoice(){
+    let str = ["Rock","Paper","Scissor"]
+    let rndm = str[Math.random() * (3)]
+    return rndm;
+}
+
+function Round(playerSelection,computerSelection){
+
+}
+
+
+function game(){
+
+}
+
+console.log(getcomputerChoice());
+console.log("Hello World");

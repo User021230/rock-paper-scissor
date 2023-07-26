7b3936173f8704d79cbc191fdeeb6d03b179d324
User021230 <mgundo48@gmail.com>
2023-07-26 23:40:44 +0200

    add logic to determine winner of the game

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function Round(playerSelection,computerSelection){
     let result = "";
     if(playerSelection==computerSelection){
         result = 2;
-    }else if((playerSelection=="scissors" && computerSelection=="paper") ||(
+    }else if((playerSelection=="scissor" && computerSelection=="paper") ||(
         playerSelection=="paper" && computerSelection=="rock") ||
         (playerSelection=="rock" && computerSelection=="scissor")){
         result = 1;
-    }else if((computerSelection=="scissors" && playerSelection=="paper") ||
+    }else if((computerSelection=="scissor" && playerSelection=="paper") ||
             (computerSelection=="paper" && playerSelection=="rock") ||
             (computerSelection=="rock" && playerSelection=="scissor")){
         result = 0;
@@ -24,13 +24,26 @@ function Round(playerSelection,computerSelection){
 function game(){
     let PlayerScore = 0;
     let ComputerScore = 0;
-    for(i = 0;i < 5; i++){
+    resume = true;
+    while(resume){
+        
         if(round()==1){
-            PlayerScore +=1; 
+            PlayerScore +=1;
+            console.log("player Score: " + PlayerScore);
         }else if(round()=2){
             ComputerScore +=1;
+            console.log("Computer Score: " + ComputerScore);
         }else if(round()==0){
             i--;
+            console.log("Draw");
+        }
+
+        if(playerScore = 5){
+            console.log("player Wins");
+            resume = false;
+        }else if(ComputerScore  = 5){
+            console.log("computer wins");
+            resume = false;
         }
     }
 

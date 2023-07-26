8e2f1ab185b8fbf1e3cb748c188bd9fb901494db
User021230 <mgundo48@gmail.com>
2023-07-26 21:52:39 +0200

    change to script tag

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getcomputerChoice(){
     let str = ["Rock","Paper","Scissor"]
-    let rndm = str[Math.random() * (3)]
+    let rndm = str[math.floor(Math.random() * 3)]
     return rndm;
 }
 
@@ -12,6 +12,6 @@ function Round(playerSelection,computerSelection){
 function game(){
 
 }
-
+console.log("hello World");
 console.log(getcomputerChoice());
 console.log("Hello World");

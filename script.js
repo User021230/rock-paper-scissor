06c4dd5ed6fe09b96ab96d00d849b38e1bfd05bc
User021230 <mgundo48@gmail.com>
2023-07-26 22:58:47 +0200

    add if statement to find result of round

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 function getcomputerChoice(){
-    let str = ["Rock","Paper","Scissor"]
-    let rndm = str[math.floor(Math.random() * 3)]
+    let str = ["Rock","Paper","scissor"]
+    let rndm = str[Math.floor(Math.random() * 3)]
     return rndm;
 }
 
 function Round(playerSelection,computerSelection){
-
+    var result = "";
+    if(playerSelection==computerSelection){
+        result = 2;
+    }else if((playerSelection=="scissors" && computerSelection=="paper") ||(
+        playerSelection=="paper" && computerSelection=="rock") ||
+        (playerSelection=="rock" && computerSelection=="scissor")){
+        result = 1;
+    }else if((computerSelection=="scissors" && playerSelection=="paper") ||
+            (computerSelection=="paper" && playerSelection=="rock") ||
+            (computerSelection=="rock" && playerSelection=="scissor")){
+        result = 0;
+    }
+    return result;
 }
 
 
 function game(){
-
+    for(i = 0;i < 5; i++){
+        if()
+    }
 }
-console.log("hello World");
-console.log(getcomputerChoice());
-console.log("Hello World");
+console.log("Hello World")
+console.log(Round("scissor",getcomputerChoice().toLowerCase()));

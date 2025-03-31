108f7cc4aa58adef8a123c44e8255087d8728726
UserO21230 <mgundo48@gmail.com>
2025-03-31 11:49:42 +0200

    rps using to button click game

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let PlayerScore = 0;
+let ComputerScore = 0;
+let count = 0
+resume = true;
+
 function getcomputerChoice(){
     let str = ["Rock","Paper","scissor"]
     let rndm = str[Math.floor(Math.random() * 3)]
@@ -21,34 +26,38 @@ function Round(playerSelection,computerSelection){
 }
 
 
-function game(){
+function game(PlayerS){
     
-    let PlayerScore = 0;
-    let ComputerScore = 0;
-    resume = true;
-    while(resume){
-        let PlayerS = prompt("Rock Paper or Scissor?").toLowerCase();
-        if(Round(PlayerS,getcomputerChoice()) == 1){
-            PlayerScore +=1;
-            console.log("player Score: " + PlayerScore);
-        }else if(Round(PlayerS,getcomputerChoice()) == 2){
-            ComputerScore +=1;
-            console.log("Computer Score: " + ComputerScore);
-        }else if(Round(PlayerS,getcomputerChoice()) == 0){
-            console.log("Draw");
-        }
+    if(!resume){
+        PlayerScore = 0;
+        ComputerScore = 0;
+        count = 0
+        document.getElementById("player-score").innerHTML = "0";
+        document.getElementById("computer-score").innerHTML = "0";
+        document.getElementById("round-res").innerHTML = "0";
+        resume = true;
+    }
+    count+= 1;
+    if(Round(PlayerS,getcomputerChoice()) == 1){
+        PlayerScore +=1;
+        document.getElementById("player-score").innerHTML = PlayerScore;
+        document.getElementById("round-res").innerHTML = "Player wins Round " + count + "!  ;)";
+    }else if(Round(PlayerS,getcomputerChoice()) == 2){
+        ComputerScore +=1;
+        document.getElementById("computer-score").innerHTML = ComputerScore;
+        document.getElementById("round-res").innerHTML = "Computer wins Round " + count + "!   [:])";
+    }else if(Round(PlayerS,getcomputerChoice()) == 0){
+        document.getElementById("round-res").innerHTML = "Round "+ count + " is a draw :(";
+    }
 
-        if(PlayerScore == 5){
-            console.log("player Wins");
-            resume = false;
-        }else if(ComputerScore == 5){
-            console.log("computer wins");
-            resume = false;
-        }
+    if(PlayerScore == 5){
+        document.getElementById("round-res").innerHTML = "<strong> PLAYER WINS <strong>"
+        resume = false;
+    }else if(ComputerScore == 5){
+        document.getElementById("round-res").innerHTML = "<strong> COMPUTER WINS <strong>"
+        resume = false;
     }
 
     
 }
 console.log("Hello World")
-// console.log(Round("scissor",getcomputerChoice().toLowerCase()));
-game();
